5047f9d7f7f40dd6f358f2acd55cdbdcdc3d5779
celsorv <vitorino.celso@gmail.com>
2023-12-02 04:23:27 -0300

    Relatório de funcionários em PDF com ReportLab

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -4,7 +4,8 @@ from .views import (
     FuncionariosList,
     FuncionarioNovo,
     FuncionarioEdit,
-    FuncionarioDelete
+    FuncionarioDelete,
+    relatorio_funcionarios
 )
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("novo/", FuncionarioNovo.as_view(), name="create_funcionario"),
     path("editar/<int:pk>/", FuncionarioEdit.as_view(), name="update_funcionario"),
     path("delete/<int:pk>/", FuncionarioDelete.as_view(), name="delete_funcionario"),
+    path("relatorio-funcionarios/", relatorio_funcionarios, name="relatorio_funcionarios"),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,4 +1,7 @@
+from io import BytesIO
+
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -6,6 +9,8 @@ from django.views.generic import (
     DeleteView,
     CreateView
 )
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
 
 from .models import Funcionario
 
@@ -31,6 +36,7 @@ class FuncionarioEdit(UpdateView):
         context["origem"] = "funcionario"
         return context
 
+
 class FuncionarioDelete(DeleteView):
     model = Funcionario
     success_url = reverse_lazy('list_funcionarios')  # volta para a listagem
@@ -48,9 +54,124 @@ class FuncionarioNovo(CreateView):
 
         # se funcionário logado é vinculado a uma empresa,
         # atribui a empresa dele ao novo funcionário
-        funcionario_criado.empresa = getattr(self.request.user.funcionario, 'empresa', None)
+        funcionario_criado.empresa = getattr(
+            self.request.user.funcionario, 'empresa', None
+        )
 
         funcionario_criado.user = User.objects.create(username=username)
 
         funcionario_criado.save()
         return super(FuncionarioNovo, self).form_valid(form)
+
+
+def create_pdf(funcionarios):
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer, pagesize=A4)
+
+    width, height = A4
+    end_x = width
+
+    # Adicione cabeçalho
+    p.drawString(200, height - 50, "Relatório de Funcionários")
+    p.line(x1=0, y1=height - 60, x2=end_x, y2=height - 60)
+
+    # Adicione detalhes dos funcionários
+    strlin = "Nome: %s | Hora Extra: %.2f"
+
+    y_start = height - 80
+    line_height = 20
+
+    for index, funcionario in enumerate(funcionarios):
+        y = y_start - index * line_height
+        p.drawString(10, y, strlin % (funcionario.nome,
+                                      funcionario.total_horas_extras)
+                     )
+
+    p.save()
+
+    pdf_data = buffer.getvalue()
+    buffer.close()
+
+    return pdf_data
+
+
+def relatorio_funcionarios(request):
+    funcionarios = Funcionario.objects.all()
+
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = 'attachment; filename="mypdf.pdf"'
+
+    # Chame a função para criar o PDF passando os funcionários
+    pdf_data = create_pdf(funcionarios)
+
+    # Escreva os dados binários diretamente na resposta
+    response.write(pdf_data)
+
+    return response
+
+# Primeiro teste básico
+# -------------------------------------
+# def pdf_reportlab(request):
+#     response = HttpResponse(content_type="application/pdf")
+#
+#     # Content-Disposition garante faz com que o arquivo seja baixado
+#     response["Content-Disposition"] = 'attachment; filename="mypdf.pdf"'
+#
+#     buffer = io.BytesIO()
+#     p = canvas.Canvas(buffer)
+#
+#     p.drawString(10, 810, "Hello world.")
+#
+#     palavras = ["palavra1", "palavra2", "palavra3"]
+#
+#     y = 790
+#     for palavra in palavras:
+#         p.drawString(10, y, palavra)
+#         y -= 40
+#
+#     p.showPage()
+#     p.save()
+#
+#     pdf = buffer.getvalue()
+#     buffer.close()
+#     response.write(pdf)
+#
+#     return response
+
+
+# Segundo teste imprimindo funcionários
+# -------------------------------------
+# def pdf_reportlab(request):
+#     response = HttpResponse(content_type="application/pdf")
+#
+#     # Content-Disposition garante faz com que o arquivo seja baixado
+#     response["Content-Disposition"] = 'attachment; filename="mypdf.pdf"'
+#
+#     buffer = io.BytesIO()
+#     p = canvas.Canvas(buffer, pagesize=A4)
+#
+#     width, height = A4
+#     end_x = width
+#
+#     p.drawString(200, 810, "Relatório de funcionários")
+#     p.line(x1=0, y1=800, x2=end_x, y2=800)
+#
+#     strlin = "Nome: %s | Hora Extra: %2.f"
+#
+#     funcionarios = Funcionario.objects.all()
+#
+#     y = 750
+#     for funcionario in funcionarios:
+#         p.drawString(10, y, strlin % (
+#             funcionario.nome, funcionario.total_horas_extras
+#         ))
+#         y -= 20
+#
+#     p.showPage()
+#     p.save()
+#
+#     pdf = buffer.getvalue()
+#     buffer.close()
+#     response.write(pdf)
+#
+#     return response
